db91d99d8cfd3c1a2cc572427f7796e0154a11f5
laxminarayan0987 <laxminarayandash20@gmail.com>
2025-02-05 12:06:21 +0530

    internships

--- src/assets/configurations/internships.ts
+++ src/assets/configurations/internships.ts
@@ -1,42 +1,42 @@
 export const internship = [
   {
     name : "Python Developer Intern",
-    description : "Learn the Fundamentals and Grow Your Generative AI (GenAI) Skill Set.",
+    description : "Dive into the world of Python, versatile and powerful programming language.",
     icon: "assets/images/artificial-intelligence4.png",
   },
   {
     name : "AI/ML Intern",
-    description : "Dive into the world of Python, versatile and powerful programming language.",
+    description : "Dive into real-world projects and shape the future of technology.",
     icon: "assets/images/artificial-intelligence.png",
   },
   {
     name : "Generative AI intern",
-    description : "Dive into the world of Python, versatile and powerful programming language.",
+    description : "Learn the Fundamentals and Grow Your Generative AI (GenAI) Skill Set.",
     icon: "assets/images/artificial-intelligence3.png",
   },
   {
     name : "Data Analyst Intern",
-    description : "Dive into the world of Python, versatile and powerful programming language.",
+    description : "Are you ready to analyze your potential?  Our Data Analyst Internship is the perfect equation for your success.",
     icon: "assets/images/data-analyst.png",
 
   },
 {
     name : "VAPT Intern",
-    description : "Dive into the world of Python, versatile and powerful programming language.",
+    description : "Are you ready to penetrate the world of cybersecurity?  Our VAPT internship is calling!",
     icon: "assets/images/testing.png",
 
   },
 
 {
     name : "Penetration Tester Intern",
-    description : "Dive into the world of Python, versatile and powerful programming language.",
+    description : "Are you ready to pentest your career? Our internship is the perfect vulnerability to exploit... with opportunity!",
     icon: "assets/images/testing2.png",
 
   },
 
   {
     name : "Security Analyst Intern",
-    description : "Become proficient in Java, a robust and widely-used programming language.",
+    description : "Are you looking for an internship that's secure a spot on our team?  We're searching for a sharp Security Analyst intern to help us protect what matters most.",
     icon: "assets/images/security.png",
 
   }
